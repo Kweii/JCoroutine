5024262f2032a4841c8de3f60e207b69b83783d5
guiliehua <guiliehua@163.com>
2018-11-10 14:23:38 +0800

    init

--- src/main/java/com/jcoroutine/common/constant/JCRConstant.java
+++ src/main/java/com/jcoroutine/common/constant/JCRConstant.java
@@ -8,7 +8,8 @@ package com.jcoroutine.common.constant;
 public class JCRConstant {
     public static final String[] KNOWN_SYS_CLASS_PKG = new String[]{
             "java.","javax.","sun.","com.sun.","javafx.","jdk.","com.oracle","oracle.jrockit.jfr.","netscape.",
-            "org.apache.","com.intellij.","org","com.google","org.checkerframework.","org.objectweb.asm.","org.junit.","junit."
+            "org.apache.","com.intellij.","org","com.google","org.checkerframework.","org.objectweb.asm.","org.junit.","junit.",
+            "com.alibaba"
     };
 
     public static final String CLS_METHOD_SEPARATOR = "_$_";
